023bd3daf7496b95739542a7cc258d55bb73fad9
“boker” <489629779@qq.com>
2024-02-25 22:49:56 +0800

    开发用户登录、注册、个人中心、修改密码功能

--- vue/src/views/Login.vue
+++ vue/src/views/Login.vue
@@ -1,25 +1,43 @@
 <template>
   <div class="container">
-    <div style="width: 400px; padding: 30px; background-color: white; border-radius: 5px;">
-      <div style="text-align: center; font-size: 20px; margin-bottom: 20px; color: #333">欢迎使用</div>
-      <el-form :model="form" :rules="rules" ref="formRef">
-        <el-form-item prop="username">
-          <el-input prefix-icon="el-icon-user" placeholder="请输入账号" v-model="form.username"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" show-password  v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button style="width: 100%; background-color: #333; border-color: #333; color: white" @click="login">登 录</el-button>
-        </el-form-item>
-<!--        <div style="display: flex; align-items: center">-->
-<!--          <div style="flex: 1"></div>-->
-<!--          <div style="flex: 1; text-align: right">-->
-<!--            还没有账号？请 <a href="/register">注册</a>-->
-<!--          </div>-->
-<!--        </div>-->
-      </el-form>
+    <div style="height: 60px; background-color: white">
+      <div style="width: 60%; height: 100%; display: flex; align-items: center; margin:  0 auto">
+        <img src="@/assets/imgs/logo.png" alt="" style="width: 40px">
+        <span style="color: #2A60C9; font-size: 24px; font-weight: bold; margin-left: 10px">社区养老系统</span>
+      </div>
     </div>
+    <div style="height: calc(100vh - 60px); width: 60%; margin:  0 auto; display: flex; align-items: center">
+      <div style="flex: 1">
+        <img src="@/assets/imgs/login.png" alt="" style="width: 100%; border-radius: 5px;">
+      </div>
+      <div style="width: 350px; padding: 50px 30px; box-shadow:  0 0 10px rgba(0, 0, 0 ,.1); background-color: white; border-radius: 5px;">
+        <div style="text-align: center; font-size: 22px; margin-bottom: 30px; font-weight: bold; color: #2A60C9">欢 迎 使 用</div>
+        <el-form :model="form" :rules="rules" ref="formRef">
+          <el-form-item prop="username">
+            <el-input prefix-icon="el-icon-user" placeholder="请输入账号" v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" show-password  v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item prop="role">
+            <el-select style="width: 100%" v-model="form.role">
+              <el-option value="ADMIN" label="管理员"></el-option>
+              <el-option value="USER" label="普通用户"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item>
+            <el-button style="width: 100%; background-color: #2A60C9; border-color: #2A60C9; color: white" @click="login">登 录</el-button>
+          </el-form-item>
+          <div style="display: flex; align-items: center">
+            <div style="flex: 1"></div>
+            <div style="flex: 1; text-align: right">
+              还没有账号？请 <a href="/register">注册</a>
+            </div>
+          </div>
+        </el-form>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -28,7 +46,7 @@ export default {
   name: "Login",
   data() {
     return {
-      form: { role: 'ADMIN' },
+      form: { role: 'USER' },
       rules: {
         username: [
           { required: true, message: '请输入账号', trigger: 'blur' },
@@ -50,8 +68,14 @@ export default {
           this.$request.post('/login', this.form).then(res => {
             if (res.code === '200') {
               localStorage.setItem("xm-user", JSON.stringify(res.data))  // 存储用户数据
-              this.$router.push('/')  // 跳转主页
               this.$message.success('登录成功')
+              setTimeout(() => {
+                if (res.data.role === 'ADMIN') {
+                  location.href = '/home'
+                } else {
+                  location.href = '/front/home'
+                }
+              }, 500)
             } else {
               this.$message.error(res.msg)
             }
@@ -67,12 +91,7 @@ export default {
 .container {
   height: 100vh;
   overflow: hidden;
-  background-image: url("@/assets/imgs/bg.jpg");
-  background-size: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #666;
+
 }
 a {
   color: #2a60c9;
--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -1,28 +1,40 @@
 <template>
   <div class="container">
-    <div style="width: 400px; padding: 30px; background-color: white; border-radius: 5px;">
-      <div style="text-align: center; font-size: 20px; margin-bottom: 20px; color: #333">欢迎注册</div>
-      <el-form :model="form" :rules="rules" ref="formRef">
-        <el-form-item prop="username">
-          <el-input prefix-icon="el-icon-user" placeholder="请输入账号" v-model="form.username"></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" show-password  v-model="form.password"></el-input>
-        </el-form-item>
-        <el-form-item prop="confirmPass">
-          <el-input prefix-icon="el-icon-lock" placeholder="请确认密码" show-password  v-model="form.confirmPass"></el-input>
-        </el-form-item>
-        <el-form-item>
-          <el-button style="width: 100%; background-color: #333; border-color: #333; color: white" @click="register">注 册</el-button>
-        </el-form-item>
-        <div style="display: flex; align-items: center">
-          <div style="flex: 1"></div>
-          <div style="flex: 1; text-align: right">
-            已有账号？请 <a href="/login">登录</a>
-          </div>
-        </div>
-      </el-form>
+    <div style="height: 60px; background-color: white">
+      <div style="width: 60%; height: 100%; display: flex; align-items: center; margin:  0 auto">
+        <img src="@/assets/imgs/logo.png" alt="" style="width: 40px">
+        <span style="color: #2A60C9; font-size: 24px; font-weight: bold; margin-left: 10px">社区养老系统</span>
+      </div>
     </div>
+    <div style="height: calc(100vh - 60px); width: 60%; margin:  0 auto; display: flex; align-items: center">
+      <div style="flex: 1">
+        <img src="@/assets/imgs/login.png" alt="" style="width: 100%; border-radius: 5px;">
+      </div>
+      <div style="width: 350px; padding: 50px 30px; box-shadow:  0 0 10px rgba(0, 0, 0 ,.1); background-color: white; border-radius: 5px;">
+        <div style="text-align: center; font-size: 22px; margin-bottom: 30px; font-weight: bold; color: #2A60C9">欢 迎 注 册</div>
+        <el-form :model="form" :rules="rules" ref="formRef">
+          <el-form-item prop="username">
+            <el-input prefix-icon="el-icon-user" placeholder="请输入账号" v-model="form.username"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input prefix-icon="el-icon-lock" placeholder="请输入密码" show-password  v-model="form.password"></el-input>
+          </el-form-item>
+          <el-form-item prop="confirmPassword">
+            <el-input prefix-icon="el-icon-lock" placeholder="请确认密码" show-password  v-model="form.confirmPassword"></el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button style="width: 100%; background-color: #2A60C9; border-color: #2A60C9; color: white" @click="register">注 册</el-button>
+          </el-form-item>
+          <div style="display: flex; align-items: center">
+            <div style="flex: 1"></div>
+            <div style="flex: 1; text-align: right">
+              已有账号？请 <a href="/register">登录</a>
+            </div>
+          </div>
+        </el-form>
+      </div>
+    </div>
+
   </div>
 </template>
 
@@ -41,7 +53,7 @@ export default {
       }
     }
     return {
-      form: {},
+      form: { role: 'USER' },
       rules: {
         username: [
           { required: true, message: '请输入账号', trigger: 'blur' },
@@ -49,7 +61,7 @@ export default {
         password: [
           { required: true, message: '请输入密码', trigger: 'blur' },
         ],
-        confirmPass: [
+        confirmPassword: [
           { validator: validatePassword, trigger: 'blur' }
         ]
       }
@@ -65,7 +77,7 @@ export default {
           // 验证通过
           this.$request.post('/register', this.form).then(res => {
             if (res.code === '200') {
-              this.$router.push('/')  // 跳转登录页面
+              this.$router.push('/login')  // 跳转登录
               this.$message.success('注册成功')
             } else {
               this.$message.error(res.msg)
@@ -82,12 +94,7 @@ export default {
 .container {
   height: 100vh;
   overflow: hidden;
-  background-image: url("@/assets/imgs/bg1.jpg");
-  background-size: 100%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  color: #666;
+  background-color: #f8f8f8;
 }
 a {
   color: #2a60c9;
