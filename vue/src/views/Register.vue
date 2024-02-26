b93156178e3c00dd98fd888034a0beeb16373f7c
“boker” <489629779@qq.com>
2024-02-26 20:19:24 +0800

    注册跳转修复和换色

--- vue/src/views/Register.vue
+++ vue/src/views/Register.vue
@@ -2,8 +2,8 @@
   <div class="container">
     <div style="height: 60px; background-color: white">
       <div style="width: 60%; height: 100%; display: flex; align-items: center; margin:  0 auto">
-        <img src="@/assets/imgs/logo.png" alt="" style="width: 40px">
-        <span style="color: #2A60C9; font-size: 24px; font-weight: bold; margin-left: 10px">社区养老系统</span>
+        <img src="@/assets/imgs/logo1.png" alt="" style="width: 40px">
+        <span style="color: #f8a23d; font-size: 24px; font-weight: bold; margin-left: 10px">长者线上服务平台</span>
       </div>
     </div>
     <div style="height: calc(100vh - 60px); width: 60%; margin:  0 auto; display: flex; align-items: center">
@@ -11,7 +11,7 @@
         <img src="@/assets/imgs/login.png" alt="" style="width: 100%; border-radius: 5px;">
       </div>
       <div style="width: 350px; padding: 50px 30px; box-shadow:  0 0 10px rgba(0, 0, 0 ,.1); background-color: white; border-radius: 5px;">
-        <div style="text-align: center; font-size: 22px; margin-bottom: 30px; font-weight: bold; color: #2A60C9">欢 迎 注 册</div>
+        <div style="text-align: center; font-size: 22px; margin-bottom: 30px; font-weight: bold; color: #f8a23d;">欢 迎 注 册</div>
         <el-form :model="form" :rules="rules" ref="formRef">
           <el-form-item prop="username">
             <el-input prefix-icon="el-icon-user" placeholder="请输入账号" v-model="form.username"></el-input>
@@ -23,12 +23,12 @@
             <el-input prefix-icon="el-icon-lock" placeholder="请确认密码" show-password  v-model="form.confirmPassword"></el-input>
           </el-form-item>
           <el-form-item>
-            <el-button style="width: 100%; background-color: #2A60C9; border-color: #2A60C9; color: white" @click="register">注 册</el-button>
+            <el-button style="width: 100%; background-color: #f8a23d; border-color: #f8a23d; color: white" @click="register">注 册</el-button>
           </el-form-item>
           <div style="display: flex; align-items: center">
             <div style="flex: 1"></div>
             <div style="flex: 1; text-align: right">
-              已有账号？请 <a href="/register">登录</a>
+              已有账号？请 <a href="/login">登录</a>
             </div>
           </div>
         </el-form>
@@ -97,6 +97,6 @@ export default {
   background-color: #f8f8f8;
 }
 a {
-  color: #2a60c9;
+  color: #f8a23d;
 }
 </style>
